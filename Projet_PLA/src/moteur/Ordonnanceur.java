f1a0514de02dc262a8a362187f8cc492027bd3ac
NASSIK Ahmed <nassahmed@live.fr>
2016-06-16 01:18:01 +0200

    Conditions eloignées prises en compte et mise en place de constantes pour les codes de conditions.

--- Projet_PLA/src/moteur/Ordonnanceur.java
+++ Projet_PLA/src/moteur/Ordonnanceur.java
@@ -7,6 +7,73 @@ import java.util.List;
 
 public class Ordonnanceur {
 	
+	final int Code_Mur=65;
+	
+	//code conditions
+	
+	//Ennemi eloigne
+	final int EnnemiEl_N=1;
+	final int EnnemiEl_E=2;
+	final int EnnemiEl_S=3;
+	final int EnnemiEl_W=4;
+	
+	//Ennemi (proche)
+	final int Ennemi_N=5;
+	final int Ennemi_E=6;
+	final int Ennemi_S=7;
+	final int Ennemi_W=8;
+	
+	//Allie (proche)
+	final int Allie_N=9;
+	final int Allie_E=10;
+	final int Allie_S=11;
+	final int Allie_W=12;
+	
+	//Case eloignee blanche
+	final int CaseElW_N=13;
+	final int CaseElW_E=14;
+	final int CaseElW_S=15;
+	final int CaseElW_W=16;
+	
+	//Case eloignee rouge
+	final int CaseElR_N=17;
+	final int CaseElR_E=18;
+	final int CaseElR_S=19;
+	final int CaseElR_W=20;
+	
+	//Case eloignee bleue
+	final int CaseElB_N=21;
+	final int CaseElB_E=22;
+	final int CaseElB_S=23;
+	final int CaseElB_W=24;
+	
+	//Case blanche (proche)
+	final int CaseW_N=25;
+	final int CaseW_E=26;
+	final int CaseW_S=27;
+	final int CaseW_W=28;
+	final int CaseW_C=29;
+	
+	//Case rouge (proche) 
+	final int CaseR_N=30;
+	final int CaseR_E=31;
+	final int CaseR_S=32;
+	final int CaseR_W=33;
+	final int CaseR_C=34;
+	
+	//Case bleue (proche) 
+	final int CaseB_N=35;
+	final int CaseB_E=36;
+	final int CaseB_S=37;
+	final int CaseB_W=38;
+	final int CaseB_C=39;
+
+	//Mur (proche)
+	final int Mur_N=40;
+	final int Mur_E=41;
+	final int Mur_S=42;
+	final int Mur_W=43;
+	
 	private Partie part;
 	
 	Ordonnanceur(Partie p){
@@ -82,11 +149,11 @@ public class Ordonnanceur {
 			
 			//Allie
 			if(p.partie().occupe(p.position().getX(), p.position().getY()-1).equipe()==p.equipe()){
-				conditions.add(9);
+				conditions.add(Allie_N);
 			}
 			else //Ennemi 
 			{
-				conditions.add(5);
+				conditions.add(Ennemi_N);
 			}
 		}
 		
@@ -95,11 +162,11 @@ public class Ordonnanceur {
 			
 			//Allie
 			if(p.partie().occupe(p.position().getX()-1, p.position().getY()).equipe()==p.equipe()){
-				conditions.add(12);
+				conditions.add(Allie_W);
 			}
 			else //Ennemi 
 			{
-				conditions.add(8);
+				conditions.add(Ennemi_W);
 			}
 		}
 		
@@ -108,11 +175,11 @@ public class Ordonnanceur {
 			
 			//Allie
 			if(p.partie().occupe(p.position().getX()+1, p.position().getY()).equipe()==p.equipe()){
-				conditions.add(10);
+				conditions.add(Allie_E);
 			}
 			else //Ennemi 
 			{
-				conditions.add(6);
+				conditions.add(Ennemi_E);
 			}
 		}
 		
@@ -121,94 +188,118 @@ public class Ordonnanceur {
 			
 			//Allie
 			if(p.partie().occupe(p.position().getX(), p.position().getY()+1).equipe()==p.equipe()){
-				conditions.add(11);
+				conditions.add(Allie_S);
 			}
 			else //Ennemi 
 			{
-				conditions.add(7);
+				conditions.add(Ennemi_S);
 			}
 		}
 		
 		//CaseBlanche(N)
 		if(p.partie().decor()[p.position().getX()][p.position().getY()-1].couleur()==0)
 		{
-			conditions.add(25);
+			conditions.add(CaseW_N);
 		}
 		else if(p.partie().decor()[p.position().getX()][p.position().getY()-1].couleur()==1)
 		//CaseBleu(N)
 		{
-			conditions.add(35);
+			conditions.add(CaseB_N);
 		}
 		else
 		//CaseRouge(N)
 		{
-			conditions.add(30);
+			conditions.add(CaseR_N);
 		}
 		
 		//CaseBlanche(E)
 		if(p.partie().decor()[p.position().getX()+1][p.position().getY()].couleur()==0)
 		{
-			conditions.add(26);
+			conditions.add(CaseW_E);
 		}
 		else if(p.partie().decor()[p.position().getX()+1][p.position().getY()].couleur()==1)
 		//CaseBleu(E)	
 		{
-			conditions.add(36);
+			conditions.add(CaseB_E);
 		}
 		else
 		//CaseRouge(E)
 		{
-			conditions.add(31);
+			conditions.add(CaseR_E);
 		}
 			
 		
 		//CaseBlanche(S)
 		if(p.partie().decor()[p.position().getX()][p.position().getY()+1].couleur()==0)
 		{
-			conditions.add(27);
+			conditions.add(CaseW_S);
 		}
 		else if(p.partie().decor()[p.position().getX()][p.position().getY()+1].couleur()==1)
 		//CaseBleu(S)
 		{
-			conditions.add(37);
+			conditions.add(CaseB_S);
 		}
 		else
 		//CaseRouge(S)
 		{
-			conditions.add(32);
+			conditions.add(CaseR_S);
 		}
 		
 		//CaseBlanche(W)
 		if(p.partie().decor()[p.position().getX()-1][p.position().getY()].couleur()==0)
 		{
-			conditions.add(28);
+			conditions.add(CaseW_W);
 		}
 		else if(p.partie().decor()[p.position().getX()-1][p.position().getY()].couleur()==1)
 		//CaseBleu(W)
 		{
-			conditions.add(38);
+			conditions.add(CaseB_W);
 		}
 		else
 		//CaseRouge(W)
 		{
-			conditions.add(33);
+			conditions.add(CaseR_W);
 		}
 		
 		//CaseBlanche(cellule actuelle)
 		if(p.partie().decor()[p.position().getX()][p.position().getY()].couleur()==0)
 		{
-			conditions.add(29);
+			conditions.add(CaseW_C);
 		}
 		else if(p.partie().decor()[p.position().getX()][p.position().getY()].couleur()==1)
 		//CaseBleu(Cellule actuelle)	
 		{
-			conditions.add(39);
+			conditions.add(CaseB_C);
 		}
 		else
 		{
-			conditions.add(34);
+			conditions.add(CaseR_C);
+		}
+		
+		
+		//Mur(N)
+		if(p.partie().decor()[p.position().getX()][p.position().getY()-1].valeur()==Code_Mur)
+		{
+			conditions.add(Mur_N);
 		}
 	
+		//Mur(E)
+		if(p.partie().decor()[p.position().getX()+1][p.position().getY()].valeur()==Code_Mur)
+		{
+			conditions.add(Mur_E);
+		}
+			
+		//Mur(S)
+		if(p.partie().decor()[p.position().getX()][p.position().getY()+1].valeur()==Code_Mur)
+		{
+			conditions.add(Mur_S);
+		}
+		
+		//Mur(W)
+		if(p.partie().decor()[p.position().getX()-1][p.position().getY()].valeur()==Code_Mur)
+		{
+			conditions.add(Mur_W);
+		}
 		
 		return conditions;
 	}
