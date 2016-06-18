045a7b373b407b16fcae4ccf3ac74818f166a1b1
NASSIK Ahmed <nassahmed@live.fr>
2016-06-18 11:54:56 +0200

    2 automates 1 guerrier et un peintre

--- Projet_PLA/automate.ml
+++ Projet_PLA/automate.ml
@@ -259,6 +259,25 @@ let tour_de_map =
     (3,CaseRouge(O),Avancer(O),3);
     (3,Mur(O),Avancer(S),0)]
   ,Guerrier);;
+
+let tour_de_map2 =
+  ([(0,CaseBlanche(S),Avancer(S),0);
+    (0,CaseBleu(S),Avancer(S),0);
+    (0,CaseRouge(S),Avancer(S),0);
+    (0,Mur(S),Avancer(E), 1);
+    (1,CaseBlanche(E),Avancer(E),1);
+    (1,CaseBleu(E),Avancer(E),1);
+    (1,CaseRouge(E),Avancer(E),1);
+    (1,Mur(E),Avancer(N),2);
+    (2,CaseBlanche(N),Avancer(N),2);
+    (2,CaseBleu(N),Avancer(N),2);
+    (2,CaseRouge(N),Avancer(N),2);
+    (2,Mur(N),Avancer(O),3);
+    (3,CaseBlanche(O),Avancer(O),3);
+    (3,CaseBleu(O),Avancer(O),3);
+    (3,CaseRouge(O),Avancer(O),3);
+    (3,Mur(O),Avancer(S),0)]
+  ,Peintre);;
 (*
 let a = traduction_automate tour_de_map;;
 let (b1,b2) = tri_aut a;;
@@ -272,7 +291,7 @@ let aut2 =
     (1,Peinte,Avancer,0)],
    Peintre);;
  *)
-let test=[tour_de_map];;
+let test=[tour_de_map;tour_de_map2];;
 
 (* TEST *)
 
